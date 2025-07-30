fea00b228b07444c4bdba6e7b8ce08207c81255f
Mr Chamster <s.duda03@gmail.com>
2025-07-31 01:04:40 +0200

    ask_path_filedialog usage in main

--- src/mainloop.py
+++ src/mainloop.py
@@ -1,2 +1,8 @@
+from src.askers import ask_path_filedialog
+
+
+
 def mainloop():
-    print("Hello main!")
+    print("Choose data file in txt format:")
+    datafile_path = ask_path_filedialog("f", "Choose data txt file")
+    print(datafile_path)
