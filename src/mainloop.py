51521323f291272440de33a8575bcc3ef2da818a
Mr Chamster <s.duda03@gmail.com>
2025-07-31 01:06:26 +0200

    Format safeguard

--- src/mainloop.py
+++ src/mainloop.py
@@ -5,4 +5,8 @@ from src.askers import ask_path_filedialog
 def mainloop():
     print("Choose data file in txt format:")
     datafile_path = ask_path_filedialog("f", "Choose data txt file")
+    if not datafile_path.endswith(".txt"):
+        print("Wrong file format")
+        return
+
     print(datafile_path)
