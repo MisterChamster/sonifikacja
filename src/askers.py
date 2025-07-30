1e18c24b3f8cb5105febfc710a4a375d8e4c4a4d
Mr Chamster <s.duda03@gmail.com>
2025-07-31 00:56:45 +0200

    ask_path_filedialog asker

---
+++ src/askers.py
@@ -0,0 +1,18 @@
+import os
+from tkinter import filedialog
+
+
+
+def ask_path_filedialog(type, message):
+    original_path = os.getcwd()
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    os.chdir(desktop_path)
+
+    sel_path = ""
+    if type == "f":
+        sel_path = filedialog.askopenfilename(title=message)
+    elif type == "d":
+        sel_path = filedialog.askdirectory(title=message)
+
+    os.chdir(original_path)
+    return sel_path
