3bfe29d720fbbe813c1f6795e74faeeaf590eece
Mr Chamster <s.duda03@gmail.com>
2025-09-30 00:03:16 +0200

    Bad and not universal hist

--- src/mainloop.py
+++ src/mainloop.py
@@ -1,5 +1,9 @@
 from src.askers import ask_path_filedialog
 import pandas as pd
+import matplotlib.pyplot as plt
+# So, a border reasonable num of putting rows in hist with loaded_boi.hist()
+# is around 25k which is 15 seconds. More than that will be a slog and we
+# don't want that.
 
 
 
@@ -12,5 +16,12 @@ def mainloop():
     print(datafile_path)
 
     loaded_boi = pd.read_csv(datafile_path, sep=" \n", engine='python', header=None)
-    # print(type(loaded_boi))
     print(loaded_boi)
+    row_count = len(loaded_boi.index)
+    print(row_count)
+    loaded_boi.hist(bins=int(row_count/10))
+
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Value')
+    plt.show()
