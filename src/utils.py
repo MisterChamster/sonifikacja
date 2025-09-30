08544f8f87a5473040bb8c6366441b307089fbca
Mr Chamster <s.duda03@gmail.com>
2025-10-01 00:05:28 +0200

    File len counting

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,7 @@ import pandas as pd
 
 
 def get_open_close_for_chunks(datafile_path: str, chunk_size: str) -> list:
-    pass
+    file_len = 0
+    with open(datafile_path, 'r') as file:
+        for _ in file:
+            file_len += 1
