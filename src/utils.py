e5c8801f9367c7879335843bc3e1e8f74ace7c08
Mr Chamster <s.duda03@gmail.com>
2025-10-01 11:52:47 +0200

    get_open_close_for_chunks upadte

--- src/utils.py
+++ src/utils.py
@@ -2,26 +2,32 @@ import pandas as pd
 
 
 
-def get_open_close_for_chunks(datafile_path: str, chunk_size: int, min_ds_val: float, max_ds_val: float) -> list:
-    ret_chunk_decider = []
-
-    file_len = 0
+def get_line_count(datafile_path: str):
+    line_count = 0
     with open(datafile_path, 'r') as file:
         for line in file:
-            file_len += 1
+            line_count += 1
             last_line = line
     if last_line:
         if last_line == "":
-            file_len -= 1
+            line_count -= 1
+    return line_count
 
-    if chunk_size > file_len:
+
+def get_open_close_for_chunks(datafile_path: str, chunk_size: int, min_ds_val: float, max_ds_val: float) -> list:
+    ret_chunk_decider = []
+
+    line_count = get_line_count(datafile_path)
+    if chunk_size > line_count:
         raise Exception("Chunk size is bigger than number of lines in the file!!")
 
+    # Equated, not fixed in case non-normalized data comes in
     mid_file_value = (min_ds_val+max_ds_val)/2
-    whole_chunks = int(file_len/chunk_size)
-    for i in range(whole_chunks):
+    whole_chunks_count = int(line_count/chunk_size)
+
+    # Analyzing chunks
+    for i in range(whole_chunks_count):
         read_start = i*chunk_size
-        # print(read_start, "Hello")
         loaded_chunk = pd.read_csv(datafile_path, header=None, names=["values"], skipinitialspace=True, skiprows=range(read_start), nrows=chunk_size)
 
         summ = 0
@@ -29,16 +35,10 @@ def get_open_close_for_chunks(datafile_path: str, chunk_size: int, min_ds_val: f
             summ += loaded_chunk["values"][i]
         avg = summ/chunk_size
 
-        # print("Mid value: ", mid_file_value, " Avg val: ", avg)
         if avg > mid_file_value:
-            # print("Greater! ", end="")
             ret_chunk_decider.append("1")
         else:
-            # print("Lower!   ", end="")
             ret_chunk_decider.append("0")
-        # print(avg)
-        # print(loaded_chunk)
-    # print(ret_chunk_decider)
     return ret_chunk_decider
 
 
